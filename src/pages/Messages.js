0605625475ee8f4ae69682c53450c255dd262ee7
ahmadshaheer1 <ashaheerki@gmail.com>
2021-09-25 18:23:19 +0430

    working on messages preview section

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,10 +1,23 @@
-import { SelectedMessagePreviewItem } from 'src/components/Messages'
+// import { SelectedMessagePreviewItem } from 'src/components/Messages'
+import { MessagePreviewItem } from 'src/components/Sidebars/Messages'
 import React from 'react'
+import profile_image from 'src/assets/feed_post_profile.png'
 
 const Messages = () => {
     return (
         <div>
-            <SelectedMessagePreviewItem />
+            <MessagePreviewItem 
+                id={2}
+                image={profile_image}
+                name="flippen.eth"
+                verified={true}
+                address="0xPras"
+                border="bottom"
+                hoverable={false}
+                settings
+                cursorPointer={false}
+            />
+            {/* <ScrollableMessages /> */}
         </div>
     )
 }
